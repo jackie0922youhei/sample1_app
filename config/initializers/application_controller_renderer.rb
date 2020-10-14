749518e717d7744d946042104e72bf729d567ed4
EC2 Default User <ec2-user@ip-172-31-11-91.ec2.internal>
2020-10-14 03:37:23 +0000

    Start dotenv-rails

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = '15ed4d0a553ae0d881dcfae48ed4ce9a8e26c55cbd3cf52147eac4219bac48d1288eaf3e10c1cabde03987aaabd466189622f2cf90651838e23851b34e59ebd7'
